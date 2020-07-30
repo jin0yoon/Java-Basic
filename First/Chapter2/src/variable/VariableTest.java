a66e747cbc631bd7abda758d9ed96b6e0daaa8bf
Yoon Jinyeong <wwwz4805@naver.com>
2020-07-30 23:23:10 +0900

    Create VariableTest.java

---
+++ First/Chapter2/src/variable/VariableTest.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class VariableTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int age; //변수 선언
+		
+		age = 30;
+		
+		System.out.println(age);
+		
+		age = 10;
+		
+//		int age = 10;  //변수 생성과 선언 동시 가능
+		
+//		int age, count; //변수 여러개 선언
+		
+	}
+	
+
+}

8a4e96e75ec2d0706e1fe0128904eb1160bf2316
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-01 19:11:07 +0900

    Create OperatorEx1

---
+++ First/Chapter2/src/variable/OperatorEx1.java
@@ -0,0 +1,23 @@
+package variable;
+
+public class OperatorEx1 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int num1 = -1;
+		int num2 = 20;
+		
+		System.out.println(+num1);
+		System.out.println(+num2);
+		
+		System.out.println(-num1);
+		System.out.println(-num2);
+		
+		System.out.println(num1);
+		System.out.println(num1);
+		
+		
+	}
+
+}

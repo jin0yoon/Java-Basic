0c525c0651f1906e96af05fe8d8e126ea284b693
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-13 21:35:56 +0900

    Create StudentTest.java

---
+++ First/oopBasic/src/classpart/StudentTest.java
@@ -0,0 +1,16 @@
+package classpart;
+
+public class StudentTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student studentLee = new Student();
+		
+		studentLee.studentName = "이순신";
+		studentLee.address = "서울";
+		
+		studentLee.showStudentInfo();
+	}
+
+}

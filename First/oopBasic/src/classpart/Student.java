c5c6c6302f2950de4da6306f0f7659a3e398a944
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-17 19:36:35 +0900

    Update Student.java
    Create 생성자

--- First/oopBasic/src/classpart/Student.java
+++ First/oopBasic/src/classpart/Student.java
@@ -7,6 +7,16 @@ public class Student {
 	public String studentName;
 	public String address;
 	
+	public Student(String name) {
+		studentName = name;
+	}
+	
+	public Student(int id, String name) {
+		studentID = id;
+		studentName = name;
+		address = "주소 없음";
+	}
+	
 	public void showStudentInfo() {
 		System.out.println(studentName + "," + address);
 	}
@@ -15,10 +25,10 @@ public class Student {
 		return studentName;
 	}
 
-	public static void main(String[] args) {
-		
-		Student studentPark = new Student();
-		studentPark.studentName = "박은종";
-		studentPark.showStudentInfo();
-	}
+//	public static void main(String[] args) {
+//		
+//		Student studentPark = new Student();
+//		studentPark.studentName = "박은종";
+//		studentPark.showStudentInfo();
+//	}
 }

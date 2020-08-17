9bb22c1baa7631574b16b8a7a2f3ad703bd669a8
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-17 19:36:45 +0900

    Update StudentTest.java

--- First/oopBasic/src/classpart/StudentTest.java
+++ First/oopBasic/src/classpart/StudentTest.java
@@ -5,16 +5,16 @@ public class StudentTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 
-		Student studentLee = new Student();
+		Student studentLee = new Student("이순신");
 		
-		studentLee.studentName = "이순신";
+//		studentLee.studentName = "이순신";
 		studentLee.address = "서울";
 		
 		studentLee.showStudentInfo();
 		
-		Student studentKim = new Student();
-		studentKim.studentName = "김유신";
-		studentKim.address = "경주";
+		Student studentKim = new Student(100, "김유신");
+//		studentKim.studentName = "김유신";
+//		studentKim.address = "경주";
 		
 		studentKim.showStudentInfo();
 		

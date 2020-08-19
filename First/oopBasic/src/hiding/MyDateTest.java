4084e9536a6efa556647e5042b1d685b22219383
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-19 19:52:53 +0900

    Create MyDateTest.java

---
+++ First/oopBasic/src/hiding/MyDateTest.java
@@ -0,0 +1,17 @@
+package hiding;
+
+public class MyDateTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		MyDate date = new MyDate();
+		
+		date.day = 10;
+		date.month = 7;
+		date.year = 2019;
+		
+		date.showDate();
+	}
+
+}

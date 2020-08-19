28086ecd5e2a119fefe0a4c48a88421b635fa7d3
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-19 19:52:45 +0900

    Create Student.java

---
+++ First/oopBasic/src/constructor/Student.java
@@ -0,0 +1,34 @@
+package constructor;
+
+
+public class Student {
+
+	public int studentID;
+	public String studentName;
+	public String address;
+	
+	public Student(String name) {
+		studentName = name;
+	}
+	
+	public Student(int id, String name) {
+		studentID = id;
+		studentName = name;
+		address = "주소 없음";
+	}
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + "," + address);
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}
+
+//	public static void main(String[] args) {
+//		
+//		Student studentPark = new Student();
+//		studentPark.studentName = "박은종";
+//		studentPark.showStudentInfo();
+//	}
+}

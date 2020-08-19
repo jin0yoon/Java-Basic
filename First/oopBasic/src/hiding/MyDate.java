58678aed33f1726d9b056f787f590e92cc5794ee
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-19 19:52:51 +0900

    Create MyDate.java

---
+++ First/oopBasic/src/hiding/MyDate.java
@@ -0,0 +1,12 @@
+package hiding;
+
+public class MyDate {
+
+	public int day;
+	public int month;
+	public int year;
+	
+	public void showDate() {
+		System.out.println(year + "년 " + month + "월 " + day + "일");
+	}
+}

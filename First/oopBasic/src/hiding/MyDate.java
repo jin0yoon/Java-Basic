efd7af4192d0c23c96ca29419305d0a725d5b5fb
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-21 19:04:03 +0900

    Update MyDate.java

--- First/oopBasic/src/hiding/MyDate.java
+++ First/oopBasic/src/hiding/MyDate.java
@@ -2,11 +2,17 @@ package hiding;
 
 public class MyDate {
 
-	public int day;
-	public int month;
-	public int year;
+//	public int day;
+//	public int month;
+//	public int year;
+	
+	int day;
+	int month;
+	int year;
 	
 	public void showDate() {
 		System.out.println(year + "년 " + month + "월 " + day + "일");
 	}
+	
+	
 }

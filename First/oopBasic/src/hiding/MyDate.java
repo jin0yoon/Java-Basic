70536a93eff9f0b28d980e9ea8fe26a26b651f37
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-22 19:07:22 +0900

    Update MyDate.java

--- First/oopBasic/src/hiding/MyDate.java
+++ First/oopBasic/src/hiding/MyDate.java
@@ -6,16 +6,50 @@ public class MyDate {
 	public int month;
 	public int year;
 	
+	private boolean isValid;
+	
 //	int day;
 //	int month;
 //	int year;
 	
 	public void setDay(int day) {
-		day = day;
+		this.day = day;
 	}
 	
+	public int getDay() {
+		return day;
+	}
+	
+	
+	public int getMonth() {
+		return month;
+	}
+
+	public void setMonth(int month) {
+		
+		if(month < 1 || month > 12) {
+			isValid = false;
+		}else {
+			isValid = true;
+		}
+		
+		this.month = month;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
 	public void showDate() {
-		System.out.println(year + "년 " + month + "월 " + day + "일");
+		if(isValid) {
+			System.out.println(year + "년 " + month + "월 " + day + "일");
+		}else {
+			System.out.println("유효하지 않은 날짜입니다.");
+		}
 	}
 	
 	

0a7907272d90631f5a1e951b9c63162b74b6cf7a
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-24 15:22:32 +0900

    Create StudentIDTest.java

---
+++ First/oopBasic/src/staticex/StudentIDTest.java
@@ -0,0 +1,22 @@
+package staticex;
+
+public class StudentIDTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println(Student.serialNum++);
+
+		Student studentLee = new Student("Lee");
+		System.out.println(studentLee.serialNum);
+		
+		Student studentKim = new Student("Kim");
+		System.out.println(studentKim.serialNum);
+		
+		System.out.println(studentLee.serialNum);
+		
+		System.out.println(studentLee.getStudentID());
+		System.out.println(studentKim.getStudentID());
+	}
+
+}

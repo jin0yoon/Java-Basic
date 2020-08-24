591230db37fe29ada216228d3e0ad4ea3608e8d5
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-24 15:22:29 +0900

    Create Student.java

---
+++ First/oopBasic/src/staticex/Student.java
@@ -0,0 +1,43 @@
+package staticex;
+
+
+public class Student {
+
+	public static int serialNum = 1000;
+	public int studentID;
+	public String studentName;
+	public String address;
+	
+	public Student(String name) {
+		studentName = name;
+		serialNum++;
+		studentID = serialNum;
+	}
+	
+	public Student(int id, String name) {
+		studentID = id;
+		studentName = name;
+		address = "주소 없음";
+		serialNum++;
+		studentID = serialNum;
+	}
+	
+	public void showStudentInfo() {
+		System.out.println(studentName + "," + address);
+	}
+	
+	public String getStudentName() {
+		return studentName;
+	}
+	
+	public int getStudentID() {
+		return studentID;
+	}
+
+//	public static void main(String[] args) {
+//		
+//		Student studentPark = new Student();
+//		studentPark.studentName = "박은종";
+//		studentPark.showStudentInfo();
+//	}
+}

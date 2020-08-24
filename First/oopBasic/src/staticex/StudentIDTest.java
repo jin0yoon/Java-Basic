dacf9ccd5f386f35308ea658e63adf1921500d1e
Yoon Jinyeong <wwwz4805@naver.com>
2020-08-24 17:08:30 +0900

    Update StudentIDTest.java

--- First/oopBasic/src/staticex/StudentIDTest.java
+++ First/oopBasic/src/staticex/StudentIDTest.java
@@ -5,7 +5,7 @@ public class StudentIDTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		System.out.println(Student.serialNum++);
+		System.out.println(Student.getSerialNum());
 
 		Student studentLee = new Student("Lee");
 		System.out.println(studentLee.serialNum);
